c9e591d51dbc00047b15dfaded6a6a0e72217daf
Paula Micheli <paula.michele.581@gmail.com>
2023-06-18 16:06:28 -0300

    Ajusta stream de video

--- src/routers/stream/stream_router.js
+++ src/routers/stream/stream_router.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParse = require("body-parser")
 const router = express.Router();
 const auth = require("../../config/auth.js")
+const fs = require('fs');
 
 router.use(bodyParse.json())
 
